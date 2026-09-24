7564c1924c152efa7fc150357e604ca2101619d3
Armani Sosa <armanibep@gmail.com>
2026-09-24 16:47:48 -0400

    feat: add user creation endpoint

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from random import randint
 
 app = FastAPI()
 
@@ -9,6 +10,10 @@ class User(BaseModel):
         email:str
         role: str
 
+class CreateUser(BaseModel):
+     name: str
+     email: str
+     role: str
 
 # Health Check
 @app.get("/health")
@@ -24,3 +29,12 @@ user_list = [{'id': 23,'name':"John Smith",'email':'john@doe.com',"role":"Lead M
 @app.get("/users",response_model=list[User])
 def get_users():
     return user_list
+
+@app.post("/users",response_model=User)
+def post_user(user: CreateUser):
+    # Generate random temp id for now
+    generate_id = randint(1,1000)
+    new_user = {'id':generate_id}
+    new_user.update(user.model_dump())
+    user_list.append(new_user)
+    return new_user
