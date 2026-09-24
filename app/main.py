b66d605f3492962404e9977941fc8c27f5a4c7b9
Armani Sosa <armanibep@gmail.com>
2026-09-24 16:09:55 -0400

    feat: add users endpoint

--- app/main.py
+++ app/main.py
@@ -2,6 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Health Check
 @app.get("/health")
 def health_check():
     return {"status":"healthy"}
+
+# In-memory test users for development purposes
+user_list = [{'id': 23,'name':"John Smith",'email':'john@doe.com',"role":"Lead Marketing"},
+             {'id': 12,'name':"Sarah Smith",'email':'jane@doe.com',"role":"Recruiter"}
+             ]
+
+# Users endpoint
+@app.get("/users")
+def get_users():
+    return user_list
