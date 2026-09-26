7afd39eaf91cbe04932b0aaee91f205e849f78e8
Armani Sosa <armanibep@gmail.com>
2026-09-26 13:12:16 -0400

    feat: create user database table

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-
+from app.models.base import Base
+from app.models.user import User # Loads user model and register its table with Base.metadata
+ 
 load_dotenv()
 database_url = os.getenv("DATABASE_URL")
 
@@ -9,4 +11,5 @@ if not database_url:
     raise ValueError("database_url should not be None")
 
 engine = create_engine(database_url)
+Base.metadata.create_all(engine)
 
